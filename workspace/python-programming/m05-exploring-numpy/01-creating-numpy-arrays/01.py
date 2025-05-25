6608309deff9b534cc20f7dd5d06772cb955ce85
naveenpn.trainer@gmail.com <naveenpn.trainer@gmail.com>
2025-05-25 15:40:56 +0530

    Code Update 25-05-2025

---
+++ workspace/python-programming/m05-exploring-numpy/01-creating-numpy-arrays/01.py
@@ -0,0 +1,6 @@
+import  numpy as np
+L = list(range(11))
+print(type(L))
+np_array = np.array(L)
+print(type(np_array))
+print(np_array)
---
+++ workspace/python-programming/m05-exploring-numpy/01-creating-numpy-arrays/02.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+v = list(range(11))
+m = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+v_arr = np.array(v)
+m_arr = np.array(m)
+print(v_arr.ndim)
+print(m_arr.ndim)
